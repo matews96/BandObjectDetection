7d1316748aadee8d3f2f5f7748710867a8fadac7
Mateo Echeverry <Mateo.X.Echeverry.-ND@disney.com>
2018-03-13 20:29:35 -0500

    martes  noche  Please enter the commit message for your changes. Lines starting

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import time
-import serial
 
 
 
@@ -18,8 +17,13 @@ import serial
 
 
 filter = np.ones((3,3))
-cammera = cv2.VideoCapture(1)
-ser = serial.Serial('/dev/cu.usbmodem1411', baudrate=9600)
+cammera = cv2.VideoCapture('out2.mp4')
+
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'avc1')
+out = cv2.VideoWriter("chocorramo.mp4", fourcc, 10.0, (800,935))
+
 
 
 def get_frame_processed(substractor = cv2.createBackgroundSubtractorMOG2(),
@@ -32,7 +36,7 @@ def get_frame_processed(substractor = cv2.createBackgroundSubtractorMOG2(),
     _, rmvBkgFrame = cv2.threshold(rmvBkgFrame, 127, 255, cv2.THRESH_BINARY)
     blurredFrame = cv2.medianBlur(rmvBkgFrame, 5)
     blurredFrame2 = cv2.erode(blurredFrame, filter, iterations = 50)
-    blurredFrame3 = cv2.dilate(blurredFrame2, filter, iterations = 50)
+    blurredFrame3 = cv2.dilate(blurredFrame2, filter, iterations = 70)
     cv2.imshow('Blurred Frame2', blurredFrame3)
 
 
@@ -56,6 +60,7 @@ def calibrate_band(calibrationDuration=10):
 
 def show_webcam():
 
+    a = np.zeros((800, 935, 3))
     threshold = calibrate_band(5) + 5
     i = 0
     initial_time = time.clock()
@@ -72,19 +77,26 @@ def show_webcam():
         average = np.average(frame)
 
         if average > threshold:
-            ser.write(b'1')
+            print("OBJETO")
+            b, _ = get_frame_processed()
+            a = b
             rect = cv2.minAreaRect(contours[0])
-            print(b'1')
             box = cv2.boxPoints(rect)
+            print(box)
+            print("--------------")
             box = np.int0(box)
+            print(box)
+            print(cv2.arcLength(box, True))
             cv2.drawContours(originalFrame, [box], 0, (255, 0, 255), 2)
             #cv2.drawContours(originalFrame, contours[0], -1, (0, 255, 0), 3)
             cv2.imshow("contours", originalFrame)
             i= i+1
+            np.save('a'+str(i), a)
             initial_time = time.clock()
         else:
-            ser.write(b'0')
-            print(b'0')
+            i = 0
+            print("NO OBJETO")
+            a = np.zeros((800, 935, 3))
             if (time.clock() - initial_time) > idle_time:
                 print("No object found for more than 3 seconds")
 
@@ -92,6 +104,12 @@ def show_webcam():
              print(i)
 
     cv2.destroyAllWindows()
+
+
+def get_properties(box, image):
+    perimeter = cv2.arcLength(box, True)
+    
+
 
 def main():
 	show_webcam()
