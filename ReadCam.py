598f84ecca58e1fe7672a4e6b460efb6d9e54d88
ECHEM025 <Mateo.X.Echeverry.-ND@disney.com>
2018-03-15 01:19:25 -0500

    automatize capture on ReadCam and get image in the center

--- ReadCam.py
+++ ReadCam.py
@@ -1,17 +1,18 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(1)
-cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap = cv2.VideoCapture(0)
 
 
 w = cap.get(cv2.CAP_PROP_FRAME_WIDTH);
 
 h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT);
 
+i = 0
+
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'avc1')
-out = cv2.VideoWriter("out2.mp4", fourcc, 10.0, (int(w),int(h)))
+out = cv2.VideoWriter("jbnjhbg.mp4", fourcc, 10.0, (int(w),int(h)))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
@@ -23,7 +24,8 @@ while(cap.isOpened()):
 
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) == 27:
-            break
+            i = i+1
+            out = cv2.VideoWriter("aadad" + str(i) + ".mp4", fourcc, 10.0, (int(w), int(h)))
     else:
         break
 
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import time
-import serial
+#import serial
 
 
 
@@ -18,8 +18,12 @@ import serial
 
 
 filter = np.ones((3,3))
-cammera = cv2.VideoCapture(1)
-ser = serial.Serial('/dev/cu.usbmodem1411', baudrate=9600)
+cammera = cv2.VideoCapture('out.mp4')
+size = (700, 935)
+a = np.zeros((size))
+#ser = serial.Serial('/dev/cu.usbmodem1411', baudrate=9600)
+range_center = np.arange(np.int0(size[0]/2-50), np.int0(size[0]/2+50), 1, np.int0)
+was_processed = False
 
 
 def get_frame_processed(substractor = cv2.createBackgroundSubtractorMOG2(),
@@ -27,13 +31,14 @@ def get_frame_processed(substractor = cv2.createBackgroundSubtractorMOG2(),
 
     ret_val, originalFrame = cammera.read()
 
-    originalFrame = originalFrame[100:900, 460:1395]
+    originalFrame = originalFrame[100:100+size[0], 460:460+size[1]]
     rmvBkgFrame = substractor.apply(originalFrame)
     _, rmvBkgFrame = cv2.threshold(rmvBkgFrame, 127, 255, cv2.THRESH_BINARY)
     blurredFrame = cv2.medianBlur(rmvBkgFrame, 5)
     blurredFrame2 = cv2.erode(blurredFrame, filter, iterations = 50)
-    blurredFrame3 = cv2.dilate(blurredFrame2, filter, iterations = 50)
-    cv2.imshow('Blurred Frame2', blurredFrame3)
+    blurredFrame3 = cv2.dilate(blurredFrame2, filter, iterations = 70)
+    #cv2.imshow('Blurred Frame2', blurredFrame3)
+    cv2.rectangle(originalFrame, (0, np.int0(size[0]/2-50)), (size[1]-1, np.int0(size[0]/2+50)), (0,255,255), thickness=2, lineType=8, shift=0)
 
 
     return originalFrame, blurredFrame3
@@ -72,19 +77,32 @@ def show_webcam():
         average = np.average(frame)
 
         if average > threshold:
-            ser.write(b'1')
+            a = np.zeros(size)
+            #ser.write(b'1')
             rect = cv2.minAreaRect(contours[0])
-            print(b'1')
+            point, _, _ = rect
+            point = np.int0(point)
+            #print(point)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
             cv2.drawContours(originalFrame, [box], 0, (255, 0, 255), 2)
+            cv2.circle(a, (point[0], point[1]), 50, 255, thickness= -1, lineType=8, shift=0)
+            Y, _ = np.nonzero(a)
+            intersection = np.intersect1d(Y, range_center)
+
+            if intersection.size > 0 and not was_processed:
+                was_processed = True
+                cv2.imshow('a', originalFrame)
+
             #cv2.drawContours(originalFrame, contours[0], -1, (0, 255, 0), 3)
+            cv2.circle(originalFrame, (point[0], point[1]), 50, (255,255,0), thickness=-1, lineType=8, shift=0)
             cv2.imshow("contours", originalFrame)
             i= i+1
             initial_time = time.clock()
         else:
-            ser.write(b'0')
-            print(b'0')
+            a = np.zeros(size)
+            was_processed = False
+            #ser.write(b'0')
             if (time.clock() - initial_time) > idle_time:
                 print("No object found for more than 3 seconds")
 
